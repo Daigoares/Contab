0c89a7186b66565759a5f3fb150eface4ceff959
Diego101101 <111402891+Daigoares@users.noreply.github.com>
2022-12-28 15:41:29 -0500

    03

--- src/js/appFunction.js
+++ src/js/appFunction.js
@@ -1,9 +1,11 @@
 const {ipcRenderer} = require("electron");
 const ipc = ipcRenderer ;
 
+
 const closeBtn1 = document.getElementById('closeBtn');
 const maxResBtn1 = document.getElementById('maxResBtn');
 const minimizeBtn1 = document.getElementById('minimizeBtn');
+
 
 closeBtn1.addEventListener('click', ()=>{
     ipc.send('closeApp')

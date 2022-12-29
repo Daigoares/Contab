421e527f714c09c95b8cc6855825a3ef3cef8277
DeCrack.diego@gmail.com <DeCrack.Diego@gmail.com>
2022-12-28 22:35:41 -0500

    03

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const url = require('url');
 const ipc = ipcMain
 
+
+
 if(process.env.NODE_ENV !== 'production'){
   require('electron-reload')(__dirname,{})
 }
@@ -11,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1000,
+    width:1000,
     height: 600,
     minWidth: 600,
     minHeight: 200,
@@ -41,7 +43,26 @@ function createWindow () {
    
     win.minimize()
   })
-}
+  ipc.on('ajustesApp', ()=>{
+  
+  const subWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  subWin.setSize(800, 600);
+  subWin.setPosition(0, 0);
+
+  subWin.loadFile('src/miniIndex.html');
+  subWin.show();
+      
+      
+  });
+  win.maximize()
+  }
 
 app.on('ready', createWindow)
 
--- src/js/appFunction.js
+++ src/js/appFunction.js
@@ -2,10 +2,60 @@ const {ipcRenderer} = require("electron");
 const ipc = ipcRenderer ;
 
 
+
+
 const closeBtn1 = document.getElementById('closeBtn');
 const maxResBtn1 = document.getElementById('maxResBtn');
 const minimizeBtn1 = document.getElementById('minimizeBtn');
+const contenedor = document.getElementById("pincipalBtns");
+const Archivos = document.getElementById("Archivos");
+const Comprobantes = document.getElementById("Comprobantes");
+const Consolidacion = document.getElementById("Consolidacion");
+const Reportes = document.getElementById("Reportes");
+const Analisis = document.getElementById("Analisis");
+const Presupuesto = document.getElementById("Presupuesto");
+const ConsolidacionB = document.getElementById("ConsolidacionB");
+const Sunat = document.getElementById("Sunat");       
+const Ajustes = document.getElementById("Ajustes");
+            
+let imagen = document.createElement("img");
+let angulo = 0;
 
+imagen.src = "icons/ajustes2.png";
+imagen.width = 20;
+imagen.height = 20;
+Ajustes.appendChild(imagen);
+Ajustes.addEventListener("mouseover", function() {
+    imagen.src = "icons/ajustes1.png";
+});
+Ajustes.addEventListener("mouseout", function() {
+    imagen.src = "icons/ajustes2.png";
+});
+
+window.addEventListener('resize',()=>{
+if(window.innerWidth >= 960){
+    Archivos.style.display = "";
+    Comprobantes.style.display = "";
+    Consolidacion.style.display = "";
+    Reportes.style.display = "";
+    Analisis.style.display = "";
+    Presupuesto.style.display = "";
+    ConsolidacionB.style.display = "";
+    Sunat.style.display = "";
+    Ajustes.style.display = "none";   
+}else{
+    Archivos.style.display = "none";
+    Comprobantes.style.display = "none";
+    Consolidacion.style.display = "none";
+    Reportes.style.display = "none";
+    Analisis.style.display = "none";
+    Presupuesto.style.display = "none";
+    ConsolidacionB.style.display = "none";
+    Sunat.style.display = "none";
+    Ajustes.style.display = ""; 
+}
+})
+            
 
 closeBtn1.addEventListener('click', ()=>{
     ipc.send('closeApp')
@@ -16,3 +66,12 @@ maxResBtn1.addEventListener('click', ()=>{
 minimizeBtn1.addEventListener('click', ()=>{
     ipc.send('minimizeApp')
 })
+Ajustes.addEventListener("click", ()=>{
+    angulo += 90;
+  if (angulo == 360) {
+    angulo = 0;
+  }
+  imagen.style.transform = "rotate(" + angulo + "deg)";
+  
+  ipc.send('ajustesApp')
+});
